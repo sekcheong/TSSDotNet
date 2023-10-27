e19ea224bcea479817f5ea4d3e9f52b47534d59f
scheong6 <scheong6@WLPF3TWHJ8>
2023-10-27 14:19:15 -0500

    add wrapkey

---
+++ Examples/Signing/Program.cs
@@ -0,0 +1,267 @@
+﻿/*
+ * Copyright (c) 2013  Microsoft Corporation
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tpm2Lib;
+
+namespace Signing
+{
+    /// <summary>
+    /// Main class to contain the program of this sample.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the argument to use to have this program use a TCP connection
+        /// to communicate with a TPM 2.0 simulator.
+        /// </summary>
+        private const string DeviceSimulator = "-tcp";
+        /// <summary>
+        /// Defines the argument to use to have this program use the Windows TBS
+        /// API to communicate with a TPM 2.0 device.
+        /// </summary>
+        private const string DeviceWinTbs = "-tbs";
+        /// <summary>
+        /// The default connection to use for communication with the TPM.
+        /// </summary>
+        private const string DefaultDevice = DeviceSimulator;
+        /// <summary>
+        /// If using a TCP connection, the default DNS name/IP address for the
+        /// simulator.
+        /// </summary>
+        private const string DefaultSimulatorName = "127.0.0.1";
+        /// <summary>
+        /// If using a TCP connection, the default TCP port of the simulator.
+        /// </summary>
+        private const int DefaultSimulatorPort = 2321;
+
+        /// <summary>
+        /// Prints instructions for usage of this program.
+        /// </summary>
+        static void WriteUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Usage: Signing [<device>]");
+            Console.WriteLine();
+            Console.WriteLine("    <device> can be '{0}' or '{1}'. Defaults to '{2}'.", DeviceWinTbs, DeviceSimulator, DefaultDevice);
+            Console.WriteLine("        If <device> is '{0}', the program will connect to a simulator\n" +
+                              "        listening on a TCP port.", DeviceSimulator);
+            Console.WriteLine("        If <device> is '{0}', the program will use the TBS interface to talk\n" +
+                              "        to the TPM device.", DeviceWinTbs);
+        }
+
+        /// <summary>
+        /// Parse the arguments of the program and return the selected values.
+        /// </summary>
+        /// <param name="args">The arguments of the program.</param>
+        /// <param name="tpmDeviceName">The name of the selected TPM connection created.</param>
+        /// <returns>True if the arguments could be parsed. False if an unknown argument or malformed
+        /// argument was present.</returns>
+        static bool ParseArguments(IEnumerable<string> args, out string tpmDeviceName)
+        {
+            tpmDeviceName = DefaultDevice;
+            foreach (string arg in args)
+            {
+                if (string.Compare(arg, DeviceSimulator, true) == 0)
+                {
+                    tpmDeviceName = DeviceSimulator;
+                }
+                else if (string.Compare(arg, DeviceWinTbs, true) == 0)
+                {
+                    tpmDeviceName = DeviceWinTbs;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// This sample demonstrates the creation of a signing "primary" key and use of this
+        /// key to sign data, and use of the TPM and TSS.Net to validate the signature.
+        /// </summary>
+        /// <param name="args">Arguments to this program.</param>
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                //
+                // Create the device according to the selected connection.
+                // 
+                Tpm2Device tpmDevice;
+
+                tpmDevice = new TbsDevice();
+
+                tpmDevice.Connect();
+
+                //
+                // Pass the device object used for communication to the TPM 2.0 object
+                // which provides the command interface.
+                // 
+                var tpm = new Tpm2(tpmDevice);
+
+
+                //
+                // AuthValue encapsulates an authorization value: essentially a byte-array.
+                // OwnerAuth is the owner authorization value of the TPM-under-test.  We
+                // assume that it (and other) auths are set to the default (null) value.
+                // If running on a real TPM, which has been provisioned by Windows, this
+                // value will be different. An administrator can retrieve the owner
+                // authorization value from the registry.
+                //
+                var ownerAuth = new AuthValue();
+
+                // 
+                // The TPM needs a template that describes the parameters of the key
+                // or other object to be created.  The template below instructs the TPM 
+                // to create a new 2048-bit non-migratable signing key.
+                // 
+                var keyTemplate = new TpmPublic(TpmAlgId.Sha1,                                  // Name algorithm
+                                                ObjectAttr.UserWithAuth | ObjectAttr.Sign | ObjectAttr.Encrypt |    // Signing key
+                                                ObjectAttr.FixedParent | ObjectAttr.FixedTPM | // Non-migratable 
+                                                ObjectAttr.SensitiveDataOrigin,
+                                                null,                                    // No policy
+                                                new RsaParms(new SymDefObject(),
+                                                             new SchemeRsassa(TpmAlgId.Sha1), 2048, 0),
+                                                new Tpm2bPublicKeyRsa());
+
+                // 
+                // Authorization for the key we are about to create.
+                // 
+                var keyAuth = new byte[] { 1, 2, 3 };
+
+                TpmPublic keyPublic;
+                CreationData creationData;
+                TkCreation creationTicket;
+                byte[] creationHash;
+
+                // 
+                // Ask the TPM to create a new primary RSA signing key.
+                // 
+                TpmHandle keyHandle = tpm[ownerAuth].CreatePrimary(
+                    TpmRh.Owner,                            // In the owner-hierarchy
+                    new SensitiveCreate(keyAuth, null),     // With this auth-value
+                    keyTemplate,                            // Describes key
+                    null,                                   // Extra data for creation ticket
+                    new PcrSelection[0],                    // Non-PCR-bound
+                    out keyPublic,                          // PubKey and attributes
+                    out creationData, out creationHash, out creationTicket);    // Not used here
+
+                // 
+                // Print out text-versions of the public key just created
+                // 
+                Console.WriteLine("New public key\n" + keyPublic.ToString());
+
+                // 
+                // Use the key to sign some data
+                // 
+                byte[] message = Encoding.Unicode.GetBytes("ABC");
+                TpmHash digestToSign = TpmHash.FromData(TpmAlgId.Sha1, message);
+
+                // 
+                // A different structure is returned for each signing scheme, 
+                // so cast the interface to our signature type (see third argument).
+                // 
+                // As an alternative, 'signature' can be of type ISignatureUnion and
+                // cast to SignatureRssa whenever a signature specific type is needed.
+                // 
+                var signature = tpm[keyAuth].Sign(keyHandle,            // Handle of signing key
+                                                  digestToSign,         // Data to sign
+                                                  null,                 // Use key's scheme
+                                                 TpmHashCheck.Null()) as SignatureRsassa;
+                // 
+                // Print the signature.
+                // 
+                Console.WriteLine("Signature: " + BitConverter.ToString(signature.sig));
+
+                var data = tpm[keyAuth].RsaEncrypt(keyHandle, message, null, null);
+                // 
+                // Use the TPM library to validate the signature
+                // 
+                bool sigOk = keyPublic.VerifySignatureOverData(message, signature);
+                if (!sigOk)
+                {
+                    throw new Exception("Signature did not validate.");
+                }
+
+                Console.WriteLine("Verified signature with TPM2lib (software implementation).");
+
+                // 
+                // Load the public key into another slot in the TPM and then 
+                // use the TPM to validate the signature
+                // 
+                TpmHandle pubHandle = tpm.LoadExternal(null, keyPublic, TpmRh.Owner);
+                tpm.VerifySignature(pubHandle, digestToSign, signature);
+                Console.WriteLine("Verified signature with TPM.");
+
+                // 
+                // The default behavior of Tpm2Lib is to create an exception if the 
+                // signature does not validate. If an error is expected the library can 
+                // be notified of this, or the exception can be turned into a value that
+                // can be later queried. The following are examples of this.
+                // 
+                signature.sig[0] ^= 1;
+                tpm._ExpectError(TpmRc.Signature)
+                   .VerifySignature(pubHandle, digestToSign, signature);
+
+                if (tpm._GetLastResponseCode() != TpmRc.Signature)
+                {
+                    throw new Exception("TPM returned unexpected return code.");
+                }
+
+                Console.WriteLine("Verified that invalid signature causes TPM_RC_SIGNATURE return code.");
+
+                // 
+                // Clean up of used handles.
+                // 
+                tpm.FlushContext(keyHandle);
+                tpm.FlushContext(pubHandle);
+
+                // 
+                // (Note that serialization is not supported on WinRT)
+                // 
+                // Demonstrate the use of XML persistence by saving keyPublic to 
+                // a file and making a copy by reading it back into a new object
+                // 
+                // NOTE: 12-JAN-2016: May be removing support for policy
+                //       serialization. We'd like to get feedback on whether
+                //       this is a desirable feature and should be retained.
+                //
+                // {
+                //     const string fileName = "sample.xml";
+                //     string xmlVersionOfObject = keyPublic.GetXml();
+                //     keyPublic.XmlSerializeToFile(fileName);
+                //     var copyOfPublic = TpmStructureBase.XmlDeserializeFromFile<TpmPublic>(fileName);
+                //     
+                //     // 
+                //     // Demonstrate Tpm2Lib support of TPM-structure equality operators
+                //     // 
+                //     if (copyOfPublic != keyPublic)
+                //     {
+                //         Console.WriteLine("Library bug persisting data.");
+                //     }
+                // }
+                //
+
+                //
+                // Clean up.
+                // 
+                tpm.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occurred: {0}", e.Message);
+            }
+
+            Console.WriteLine("Press Any Key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Examples/WrapKey/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tpm2Lib;
+
+namespace WrapKey
+{
+    class Program
+    {
+        /// <summary>
+        /// Creates a primary RSA storage key in the storage hierarchy and returns its
+        /// handle. The caller can provide an auth value and additional entropy for
+        /// the key derivation (primary keys are deterministically derived by the TPM
+        /// from an internal primary seed value unique for each hierarchy).
+        /// The caller is responsible for disposing of the returned key handle.
+        /// </summary>
+        /// <param name="auth">Optional auth value to be associated with the created key.</param>
+        /// <param name="seed">Optional entropy that may be used to create different primary keys with exactly the same template.</param>
+        /// <returns></returns>
+        static TpmHandle CreateRsaPrimaryStorageKey(Tpm2 tpm, byte[] auth = null, byte[] seed = null)
+        {
+            TpmPublic newKeyPub;
+            return CreateRsaPrimaryStorageKey(tpm, out newKeyPub, seed, auth);
+        }
+
+        /// <summary>
+        /// Creates a primary RSA storage key in the storage hierarchy and returns its
+        /// handle and public area. The caller can provide an auth value and additional
+        /// entropy for the key derivation (primary keys are deterministically derived
+        /// by the TPM from an internal primary seed value unique for each hierarchy).
+        /// The caller is responsible for disposing of the returned key handle.
+        /// </summary>
+        /// <param name="newKeyPub">Public area of the created key. Its 'unique' member contains the actual public key of the generated key pair.</param>
+        /// <param name="auth">Optional auth value to be associated with the created key.</param>
+        /// <param name="seed">Optional entropy that may be used to create different primary keys with exactly the same template.</param>
+        /// <returns></returns>
+        static TpmHandle CreateRsaPrimaryStorageKey(Tpm2 tpm, out TpmPublic newKeyPub, byte[] auth = null, byte[] seed = null)
+        {
+            // auth data provided by caller, no private key bits for asymmetric keys
+            var sensCreate = new SensitiveCreate(auth, null);
+
+            // Typical storage key template
+            var parms = new TpmPublic(
+                TpmAlgId.Sha256,                                 
+                ObjectAttr.Restricted | ObjectAttr.Decrypt |      // Storage key
+                ObjectAttr.FixedParent | ObjectAttr.FixedTPM |    // Non-duplicable
+                ObjectAttr.UserWithAuth | ObjectAttr.SensitiveDataOrigin,
+                null,                                             
+                new RsaParms(new SymDefObject(TpmAlgId.Aes, 128, TpmAlgId.Cfb), null, 2048, 0),
+                new Tpm2bPublicKeyRsa(seed)    
+            );
+
+            CreationData creationData;
+            TkCreation creationTicket;
+            byte[] creationHash;
+
+            return tpm.CreatePrimary(
+                TpmRh.Owner,          
+                sensCreate,                                                
+                parms,    
+                null,         
+                new PcrSelection[0],  // Not PCR-bound
+                out newKeyPub,        // Our outs
+                out creationData, out creationHash, out creationTicket);
+        } 
+
+        static byte[] Encrypt(Tpm2 tpm, TpmPublic keyPub, TpmHandle hParent, byte[] message, out byte[] iv)
+        {
+            var keyAuth = AuthValue.FromString(TpmAlgId.Sha256, "lotsofcats");         
+            var key = TssObject.Create(keyPub, keyAuth);
+
+            iv = null;
+            return null;
+        }
+
+        static byte[] Decrypt(Tpm2 tpm, TpmPublic keyPub, TpmHandle hParent, byte[] cipher, byte[] iv)
+        {
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            // Connect to the simulator on this machine    
+            var device = new TbsDevice();
+            device.Connect();
+            Tpm2 tpm = new Tpm2(device);
+
+            var hPrim = CreateRsaPrimaryStorageKey(tpm);
+
+            var keyPub = new TpmPublic(
+                        TpmAlgId.Sha256,
+                        ObjectAttr.Decrypt | ObjectAttr.UserWithAuth,
+                        null,
+                        new SymDefObject(TpmAlgId.Aes, 256, TpmAlgId.Cfb),
+                        new Tpm2bDigestSymcipher()
+                        );
+
+            var keyAuth = AuthValue.FromString(TpmAlgId.Sha256, "lotsofcats");
+
+            // Generate the key
+            TssObject swKey = TssObject.Create(keyPub, keyAuth);
+
+
+
+            tpm.Dispose();
+        }
+    }
+}
