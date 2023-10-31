a43cd8461da0ce4dd513c784752602104265e64f
Cheong, Sek [ETHUS] <scheong6@WLPF3TWHJ8>
2023-10-31 10:54:55 -0500

    add wrapkey

--- Examples/WrapKey/Program.cs
+++ Examples/WrapKey/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Tpm2Lib;
@@ -8,63 +10,7 @@ namespace WrapKey
 {
     class Program
     {
-        /// <summary>
-        /// Creates a primary RSA storage key in the storage hierarchy and returns its
-        /// handle. The caller can provide an auth value and additional entropy for
-        /// the key derivation (primary keys are deterministically derived by the TPM
-        /// from an internal primary seed value unique for each hierarchy).
-        /// The caller is responsible for disposing of the returned key handle.
-        /// </summary>
-        /// <param name="auth">Optional auth value to be associated with the created key.</param>
-        /// <param name="seed">Optional entropy that may be used to create different primary keys with exactly the same template.</param>
-        /// <returns></returns>
-        static TpmHandle CreateRsaPrimaryStorageKey(Tpm2 tpm, byte[] auth = null, byte[] seed = null)
-        {
-            TpmPublic newKeyPub;
-            return CreateRsaPrimaryStorageKey(tpm, out newKeyPub, seed, auth);
-        }
-
-        /// <summary>
-        /// Creates a primary RSA storage key in the storage hierarchy and returns its
-        /// handle and public area. The caller can provide an auth value and additional
-        /// entropy for the key derivation (primary keys are deterministically derived
-        /// by the TPM from an internal primary seed value unique for each hierarchy).
-        /// The caller is responsible for disposing of the returned key handle.
-        /// </summary>
-        /// <param name="newKeyPub">Public area of the created key. Its 'unique' member contains the actual public key of the generated key pair.</param>
-        /// <param name="auth">Optional auth value to be associated with the created key.</param>
-        /// <param name="seed">Optional entropy that may be used to create different primary keys with exactly the same template.</param>
-        /// <returns></returns>
-        static TpmHandle CreateRsaPrimaryStorageKey(Tpm2 tpm, out TpmPublic newKeyPub, byte[] auth = null, byte[] seed = null)
-        {
-            // auth data provided by caller, no private key bits for asymmetric keys
-            var sensCreate = new SensitiveCreate(auth, null);
-
-            // Typical storage key template
-            var parms = new TpmPublic(
-                TpmAlgId.Sha256,                                 
-                ObjectAttr.Restricted | ObjectAttr.Decrypt |      // Storage key
-                ObjectAttr.FixedParent | ObjectAttr.FixedTPM |    // Non-duplicable
-                ObjectAttr.UserWithAuth | ObjectAttr.SensitiveDataOrigin,
-                null,                                             
-                new RsaParms(new SymDefObject(TpmAlgId.Aes, 128, TpmAlgId.Cfb), null, 2048, 0),
-                new Tpm2bPublicKeyRsa(seed)    
-            );
-
-            CreationData creationData;
-            TkCreation creationTicket;
-            byte[] creationHash;
-
-            return tpm.CreatePrimary(
-                TpmRh.Owner,          
-                sensCreate,                                                
-                parms,    
-                null,         
-                new PcrSelection[0],  // Not PCR-bound
-                out newKeyPub,        // Our outs
-                out creationData, out creationHash, out creationTicket);
-        } 
-
+   
 
         static byte[] GetKeyFromTPM()
         {
@@ -77,37 +23,29 @@ namespace WrapKey
             //
             // Pass the device object used for communication to the TPM 2.0 object
             // which provides the command interface.
-            // 
             var tpm = new Tpm2(tpmDevice);
 
-
-            //
             // AuthValue encapsulates an authorization value: essentially a byte-array.
             // OwnerAuth is the owner authorization value of the TPM-under-test.  We
             // assume that it (and other) auths are set to the default (null) value.
             // If running on a real TPM, which has been provisioned by Windows, this
             // value will be different. An administrator can retrieve the owner
-            // authorization value from the registry.
-            //
             var ownerAuth = new AuthValue();
 
-            // 
             // The TPM needs a template that describes the parameters of the key
             // or other object to be created.  The template below instructs the TPM 
             // to create a new 2048-bit non-migratable signing key.
-            // 
             var keyTemplate = new TpmPublic(TpmAlgId.Sha1,                                  // Name algorithm
                                             ObjectAttr.UserWithAuth | ObjectAttr.Sign | ObjectAttr.Encrypt |    // Signing key
                                             ObjectAttr.FixedParent | ObjectAttr.FixedTPM | // Non-migratable 
                                             ObjectAttr.SensitiveDataOrigin,
                                             null,                                    // No policy
                                             new RsaParms(new SymDefObject(),
-                                                         new SchemeRsassa(TpmAlgId.Sha1), 2048, 0),
+                                                         new SchemeRsassa(TpmAlgId.Sha1), 1024, 0),
                                             new Tpm2bPublicKeyRsa());
 
             // 
             // Authorization for the key we are about to create.
-            // 
             var keyAuth = new byte[] { 1, 2, 3 };
 
             TpmPublic keyPublic;
@@ -127,14 +65,7 @@ namespace WrapKey
                 out keyPublic,                          // PubKey and attributes
                 out creationData, out creationHash, out creationTicket);    // Not used here
 
-            // 
-            // Print out text-versions of the public key just created
-            // 
-            Console.WriteLine("New public key\n" + keyPublic.ToString());
-
-            // 
-            // Use the key to sign some data
-            // 
+           
             byte[] message = Encoding.Unicode.GetBytes("ABC");
             TpmHash digestToSign = TpmHash.FromData(TpmAlgId.Sha1, message);
 
@@ -149,33 +80,116 @@ namespace WrapKey
                                               digestToSign,         // Data to sign
                                               null,                 // Use key's scheme
                                              TpmHashCheck.Null()) as SignatureRsassa;
-            // 
-            // Print the signature.
-            // 
-            Console.WriteLine("Signature: " + BitConverter.ToString(signature.sig));
-
             return signature.sig;
         }
 
-        static byte[] Encrypt(Tpm2 tpm, TpmPublic keyPub, TpmHandle hParent, byte[] message, out byte[] iv)
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
         {
-            var keyAuth = AuthValue.FromString(TpmAlgId.Sha256, "lotsofcats");         
-            var key = TssObject.Create(keyPub, keyAuth);
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
 
-            iv = null;
-            return null;
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
         }
 
-        static byte[] Decrypt(Tpm2 tpm, TpmPublic keyPub, TpmHandle hParent, byte[] cipher, byte[] iv)
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
         {
-            return null;
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
         }
 
         static void Main(string[] args)
         {
-            var k = GetKeyFromTPM();
-            var txt = Convert.ToBase64String(k,0,k.Length);
-            Console.WriteLine(txt);
+            var key = GetKeyFromTPM();
+
+            byte[] aeskey = new byte[32];
+            Array.Copy(key, aeskey, 32);
+            var txt = Convert.ToBase64String(aeskey, 0, aeskey.Length);
+            Console.WriteLine("Key:" + txt);
+
+            //string ivstr = "7E892875A52C59A3B588306B13C31FBD";
+            byte[] iv = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+            string original = "Here is some data to encrypt!";
+
+            // Encrypt the string to an array of bytes.
+            byte[] encrypted = EncryptStringToBytes(original, aeskey, iv);
+
+            // Decrypt the bytes to a string.
+            string roundtrip = DecryptStringFromBytes(encrypted, aeskey, iv);
+
+            //Display the original data and the decrypted data.
+            Console.WriteLine("Original:   {0}", original);
+            Console.WriteLine("Round Trip: {0}", roundtrip);
+
         }
     }
 }
